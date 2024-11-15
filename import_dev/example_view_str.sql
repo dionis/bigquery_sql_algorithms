5ea321ca4dee7b2e2b838134809e6c81d80cbd2d
Dionis López Ramos <inoid2007@gmail.com>
2024-11-15 11:56:40 -0500

    Update withow name of Bigquery project

--- import_dev/example_view_str.sql
+++ import_dev/example_view_str.sql
@@ -1,13 +1,13 @@
 WITH Customer AS (
-SELECT  *, 0 AS EXAMPLE, '0' AS EXAMPLE1 FROM `gen-lang-client-0781718982.import_dev.Customer` LIMIT 5
+SELECT  *, 0 AS EXAMPLE, '0' AS EXAMPLE1 FROM `import_dev.Customer` LIMIT 5
 ),
 
 Cashier AS (
-SELECT * FROM `gen-lang-client-0781718982.import_dev.cashier` LIMIT 5
+SELECT * FROM `import_dev.cashier` LIMIT 5
 ),
 
 Promotion AS (
-SELECT *, 'Test' AS EXAMPLE, 'Test2' AS EXAMPLE1,  'Test3' AS EXAMPLE3, 'Test4' AS EXAMPLE4 FROM `gen-lang-client-0781718982.import_dev.promotion` LIMIT 5
+SELECT *, 'Test' AS EXAMPLE, 'Test2' AS EXAMPLE1,  'Test3' AS EXAMPLE3, 'Test4' AS EXAMPLE4 FROM `import_dev.promotion` LIMIT 5
 )
 
 Select * FROM 
