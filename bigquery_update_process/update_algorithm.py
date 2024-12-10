b78adbbd1989d19988c02e96fa1f0b21807f6c80
Dionis López Ramos <inoid2007@gmail.com>
2024-12-09 21:25:53 -0500

    Update new  step for update test table

--- bigquery_update_process/update_algorithm.py
+++ bigquery_update_process/update_algorithm.py
@@ -1,39 +1,62 @@
 import os
+#from gbq import BigQuery
+#from pora_bigquery import PoraBigquery
 
 bigquery_datasets = 'bigquery_update_process/test_pora_bigquery/'
-MAX_FILES_IN_ALGORITMDIR = 2
-ERROR_MAX_FILES_IN_ALGORITMDIR = 'A size of files more than permit in algorithm directory'
+MAX_FILES_IN_ALGORITMDIR = 3
+ERROR_MAX_FILES_IN_ALGORITMDIR = 'A size of files more than permit in algorithm directory.'
 TABLE_NAME = 'tables'
-ERROR_TABLE_NAME_NOT_FOUND = 'The name of table directory is not corret or not exist'
-LIMIT_FILE_INTABLE = 1
-ERROR_LIMIT_FILE_INTABLE = 'The limit of files in table directory is more than permit'
+ERROR_TABLE_NAME_NOT_FOUND = 'The name of table directory is not corret or not exist.'
+LIMIT_FILE_INTABLE = 5
+ERROR_LIMIT_FILE_INTABLE = 'The limit of files in table directory is more than permit.'
 CORRECT_TABLE_EXTENSION = 'csv'
-ERROR_CORRECT_TABLE_EXTENSION = 'The file for temporal table is not correct '
+ERROR_CORRECT_TABLE_EXTENSION = 'The file for temporal table is not correct.'
+DOC_DIRECTORY = "WP"
+
+SQL_DROP_TABLE = "DROP TABLE IF EXISTS "
 
 def process_source_code_in_BigQuery(bigquery_dataset:str, process_algorithm: dict):
-        print(f"Information about PORA algorithm source code for dataset {bigquery_dataset}")
+        print(f"*** Information about PORA algorithm source code for dataset {bigquery_dataset} ***\n")
         #print(process_algorithm)
 
         table_name = csv_file_toload_bigquery = ''
 
+        dataset_schema_directory = os.environ.get("dataset_schema_directory")
+        credentials = os.environ.get("credentials")
+        gcp_project = os.environ.get("gcp_project")
+
+        bq = PoraBigquery(svc_account=svc_account, project = gcp_project)
+
         # 1- Create temporal table in Biguery squeme
         if TABLE_NAME in process_algorithm:
+        
+            #Extract information from table directory
+            list_of_tables = process_algorithm[TABLE_NAME]
+            for iTable in list_of_tables:
+                table_name,  csv_file_toload_bigquery = iTable
+                print(f"Step 0 - Drpo table {table_name} if exist  `{SQL_DROP_TABLE} {table_name}`\n")
+
+                bq.execute(
+                        f"{SQL_DROP_TABLE} {table_name}"
+                    )
+
+                print(f"Step 1 - Create table {table_name} in BigQuery using csv source code {csv_file_toload_bigquery}\n")
+                
+                bq.bigquery_import_csv(csv_file_toload_bigquery, table_name)
+            
             #erase in dictornary
-            table_name,  csv_file_toload_bigquery = process_algorithm[TABLE_NAME]
-            print(f"Step 0 - Create table {table_name} in BigQuery using csv source code {csv_file_toload_bigquery}")
-            #bq.create_table_from_csv(gcp_project, dataset, table_name, csv_file_toload_bigquery)
             del process_algorithm[TABLE_NAME]
         
         #Update algoritm code in Bigquery
         for key, ialgorithm_source_address in process_algorithm.items():
-            print(f"Step 1- Create or Update PORA algorithm {key} in Bigquery with file in address {ialgorithm_source_address}")
+            print(f"Step 2- Create or Update PORA algorithm {key} in Bigquery with file in address {ialgorithm_source_address}\n")
 
             #Execute algoritm for testing propouse
-            print(f"Step 2- Execute PORA algorithm {key} in Bigquery with file in address {ialgorithm_source_address}")
+            print(f"Step 3- Execute PORA algorithm {key} in Bigquery with file in address {ialgorithm_source_address}\n")
 
         #Delete temporal table
         if table_name != '':
-          print(f"Setep 3- Delete temporal table {table_name} in BigQuery")
+          print(f"Setep 4- Delete temporal table {table_name} in BigQuery\n")
           #bq.delete_table(gcp_project, dataset, table_name)
         
         # 2- Import data from CSV file to temporal table in BigQuery
@@ -45,7 +68,7 @@ def process_source_code_in_BigQuery(bigquery_dataset:str, process_algorithm: dic
 
 
 def process_algorithm_codesource(bigquery_dataset: str, algorithm_name: str, algoritm_source_address:str):
-    print(f"In file addres {algoritm_source_address}")   
+    #print(f"In file addres {algoritm_source_address}")   
 
     list_of_files = os.listdir(algoritm_source_address)
     size_of_files = len(list_of_files)
@@ -56,9 +79,9 @@ def process_algorithm_codesource(bigquery_dataset: str, algorithm_name: str, alg
         process_algorithm = {}
 
         for ifiles in  os.listdir(algoritm_source_address):
-            source_path = algoritm_source_address + os.sep + ifiles
+            source_path = algoritm_source_address + os.sep + ifiles  
 
-            if os.path.isdir(source_path):
+            if os.path.isdir(source_path) and ifiles != DOC_DIRECTORY:
                 directory_name =  source_path.split(os.sep)[-1]
 
                 if directory_name != TABLE_NAME:
@@ -70,13 +93,22 @@ def process_algorithm_codesource(bigquery_dataset: str, algorithm_name: str, alg
                     if len(list_source_code_intable) > LIMIT_FILE_INTABLE:
                         raise Exception(ERROR_LIMIT_FILE_INTABLE)
                     else: #Only a file with csv extension
-                        file_name_and_extension = list_source_code_intable[0].split(".")      
+                        for ifile_intable in list_source_code_intable:
+                            file_name_and_extension = ifile_intable.split(".")      
 
-                        if (file_name_and_extension[1] != CORRECT_TABLE_EXTENSION):
-                            raise Exception(f"{ERROR_CORRECT_TABLE_EXTENSION}: {file_name_and_extension[1]}")
-                        else:
-                            process_algorithm[TABLE_NAME] =(file_name_and_extension[0],  source_path + os.sep + list_source_code_intable[0])
-                    
+                            if (file_name_and_extension[1] != CORRECT_TABLE_EXTENSION):
+                                raise Exception(f"{ERROR_CORRECT_TABLE_EXTENSION}: {file_name_and_extension[1]}")
+                            else:
+                                if  TABLE_NAME not in process_algorithm:
+                                   process_algorithm[TABLE_NAME] = [(
+                                                file_name_and_extension[0],  
+                                                source_path + os.sep + list_source_code_intable[0]
+                                            )]
+                                else:
+                                    process_algorithm[TABLE_NAME].append((
+                                                file_name_and_extension[0],  
+                                                source_path + os.sep + list_source_code_intable[0]
+                                            ))
                 
             elif os.path.isfile(source_path):
                 file_name_and_extension = ifiles.split(".")      
